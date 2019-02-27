4b778d408c05ebef0276c30d9827c0583b3f26e6
HarryBatchelor <batchelorh@cardiff.ac.uk>
2019-02-27 20:33:01 +0000

    Finished summative questions

--- SummativeTest.py
+++ SummativeTest.py
@@ -6,6 +6,8 @@ def main():
     notebook.add(frame1, text="One")
     notebook.add(frame2, text="Two")
     notebook.add(frame3, text="Three")
+    notebook.add(frame4, text="Four")
+    notebook.add(frame5, text="Five")
 
     Label(frame1, text="What is Tkinter?").grid(row=2, column=2)
     Button(frame1, text="Guided User Interface", command=correct1).grid(row=3, column=1)
@@ -21,6 +23,16 @@ def main():
     Button(frame3, text="Create a window", command=incorrect3).grid(row=3, column=1)
     Button(frame3, text="Show a message in the Python Shell", command=correct3).grid(row=3, column=2)
     Button(frame3, text="Print to the printer", command=incorrect3).grid(row=3, column=3)
+
+    Label(frame4, text="What is Sublime Text?").grid(row=2, column=2)
+    Button(frame4, text="A text editor", command=correct4).grid(row=3, column=1)
+    Button(frame4, text="A music player", command=incorrect4).grid(row=3, column=2)
+    Button(frame4, text="A word processor", command=incorrect4).grid(row=3, column=3)
+
+    Label(frame5, text="What type of language is python?").grid(row=2, column=2)
+    Button(frame5, text="A low level language", command=incorrect5).grid(row=3, column=1)
+    Button(frame5, text="An object orientated language", command=incorrect5).grid(row=3, column=2)
+    Button(frame5, text="A high level language", command=correct5).grid(row=3, column=3)
 
     notebook.pack()
 
@@ -48,6 +60,20 @@ def correct3():
 def incorrect3():
     Label(frame3, text="Incorrect").grid(row=1, column=2)
 
+def correct4():
+    Label(frame4, text="Correct").grid(row=1, column=2)
+    counter()
+
+def incorrect4():
+    Label(frame4, text="Incorrect").grid(row=1, column=2)
+
+def correct5():
+    Label(frame5, text="Correct").grid(row=1, column=2)
+    counter()
+
+def incorrect5():
+    Label(frame5, text="Correct").grid(row=1, column=2)
+
 def counter():
     total.set(total.get() + 1)
 
@@ -60,6 +86,8 @@ notebook = ttk.Notebook(root)
 frame1 = ttk.Frame(notebook)
 frame2 = ttk.Frame(notebook)
 frame3 = ttk.Frame(notebook)
+frame4 = ttk.Frame(notebook)
+frame5 = ttk.Frame(notebook)
 
 main()
 
