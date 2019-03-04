0b889c4a6195554af5ab665f84c6cfd29f29374c
Harry Batchelor <batchelorh@cardiff.ac.uk>
2019-03-04 11:06:24 +0000

    Change to Student

--- StudentPage.py
+++ StudentPage.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 
 class Window(Frame):
@@ -43,50 +42,3 @@ root.geometry("400x300")
 app = Window(root)
 
 root.mainloop
-    
-
-from tkinter import *
-
-class Window(Frame):
-
-    def __init__(self, master = None):
-        Frame.__init__(self, master)
-
-        self.master = master
-
-        self.init_window()
-
-    def init_window(self):
-
-        self.master.title("Student Page")
-
-        self.pack(fill=BOTH, expand=1)
-
-        sumButton = Button(self, text="Summative Assessment")
-
-        sumButton.place(x=10, y=10)
-
-        formButton = Button(self, text="Formative Assessment")
-
-        formButton.place(x=10, y=40)
-
-        resButton = Button(self, text="Results")
-
-        resButton.place(x=10, y=70)
-
-        gobackButton = Button(self, text="Go Back")
-
-        gobackButton.place(x=167, y=240)
-
-
-
-
-
-
-root = Tk()#Tk is part of Tkinter, calls the root the main window
-root.geometry("400x300")
-
-app = Window(root)
-
-root.mainloop
-
